a7105eecb9fe0dd019090099109332208b13011e
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-05-11 21:47:35 -0300

    First commit user-api

---
+++ user-api/src/test/java/carreiras/com/github/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package carreiras.com.github.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
