e97700ee0d564494107153269f8fabda2c84d2e1
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-05-17 22:23:00 -0300

    Serviço de Usuários

--- user-api/src/main/java/carreiras/com/github/userapi/controller/UserController.java
+++ user-api/src/main/java/carreiras/com/github/userapi/controller/UserController.java
@@ -2,7 +2,12 @@ package carreiras.com.github.userapi.controller;
 
 import carreiras.com.github.userapi.dto.UserDTO;
 import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,17 +15,40 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     public static List<UserDTO> usuarios = new ArrayList<>();
 
-    @GetMapping("/")
-    public String getMensagem() {
-        return "Spring boot está funcionando";
-    }
-
-    @GetMapping("/users")
+    @GetMapping
     public List<UserDTO> getUsers() {
         return usuarios;
+    }
+
+    @GetMapping("/{cpf}")
+    public UserDTO getUsersFiltro(@PathVariable String cpf) {
+        for (UserDTO userFilter : usuarios)
+            if (userFilter.getCpf().equals(cpf))
+                return userFilter;
+
+        return null;
+    }
+
+    @PostMapping
+    UserDTO include(@RequestBody UserDTO userDTO) {
+        userDTO.setDataCadastro(new Date());
+        usuarios.add(userDTO);
+        return userDTO;
+    }
+
+    @DeleteMapping("/{cpf}")
+    public boolean delete(@PathVariable String cpf) {
+        for (UserDTO userFilter : usuarios)
+            if (userFilter.getCpf().equals(cpf)) {
+                usuarios.remove(userFilter);
+                return true;
+            }
+
+        return false;
     }
 
     @PostConstruct
