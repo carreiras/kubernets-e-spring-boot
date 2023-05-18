2ac0b1916aa667be15ff812266e1ceee83bc6f9f
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-05-18 15:49:15 -0300

    First commit product-api

---
+++ product-api/src/test/java/carreiras/github/com/productapi/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package carreiras.github.com.productapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
