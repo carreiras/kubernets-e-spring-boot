0d23f54067bc86c2869cf8d41bc550947e8d07fc
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-05-18 16:30:42 -0300

    Camada de controladores

---
+++ product-api/src/main/java/carreiras/github/com/productapi/controller/ProductController.java
@@ -0,0 +1,48 @@
+package carreiras.github.com.productapi.controller;
+
+import carreiras.github.com.productapi.dto.ProductDTO;
+import carreiras.github.com.productapi.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/product")
+    public List<ProductDTO> getProducts() {
+        List<ProductDTO> produtos = productService.getAll();
+        return produtos;
+    }
+
+    @GetMapping("/product/category/{categoryId}")
+    public List<ProductDTO> getProductByCategory(@PathVariable Long categoryId) {
+        List<ProductDTO> produtos = productService.getProductByCategoryId(categoryId);
+        return produtos;
+    }
+
+    @GetMapping("/product/{productIdentifier}")
+    ProductDTO findById(@PathVariable String productIdentifier) {
+        return productService.findByProductIdentifier(productIdentifier);
+    }
+
+    @PostMapping("/product")
+    ProductDTO newProduct(@Valid @RequestBody ProductDTO productDTO) {
+        return productService.save(productDTO);
+    }
+
+    @DeleteMapping("/product/{id}")
+    void delete(@PathVariable Long id) /*throws ProductNotFoundException*/ {
+        productService.delete(id);
+    }
+}
