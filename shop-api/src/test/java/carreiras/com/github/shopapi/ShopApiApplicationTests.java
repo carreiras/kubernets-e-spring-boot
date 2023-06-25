b625b00c6bc1da25f382072fc4021cb1ccfde6c1
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-06-25 14:54:22 -0300

    Criação shop-api

---
+++ shop-api/src/test/java/carreiras/com/github/shopapi/ShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package carreiras.com.github.shopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
